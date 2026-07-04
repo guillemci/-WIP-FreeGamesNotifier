47fe57352b11266e11e62d4e638439a7b9f0e577
guillemci <guillemcib@gmail.com>
2026-07-04 03:27:11 +0200

    almost all refactored, still missing, relations builder, better Naming and others..., but the project is way cleaner

--- Backend-GameDiscountNotifier/BackGround/BackgroundServiceAdquireFreeGamesJSON.cs
+++ Backend-GameDiscountNotifier/BackGround/BackgroundServiceAdquireFreeGamesJSON.cs
@@ -1,4 +1,5 @@
 ﻿using Backend_GameDiscountNotifier.Data;
+using Backend_GameDiscountNotifier.Logic;
 using Backend_GameDiscountNotifier.Model.Contet;
 using System;
 using System.Collections.Generic;
@@ -51,138 +52,31 @@ namespace Backend_GameDiscountNotifier.BackGround
                 //la majoria del codi sera refactoritzat en metodes mes petits per una millor llegibilitat
                 foreach (var valor in contingut.EnumerateArray())
                 {
-                    if (valor.GetProperty("promotions").ValueKind != JsonValueKind.Null)
+                    if 
+                    (
+                        valor.GetProperty("promotions").ValueKind != JsonValueKind.Null || 
+                        ofertas.Any(e => e.IdExtretOferta == valor.GetProperty("id").ToString())
+                    )
                     {
                         SellerJoc sellerJocTemp = new();
                         Joc jocTemp = new();
                         JocEnPlataforma jocEnPlataformaTemp = new();
                         Oferta ofertaTemp = new();
-                        var tipusOferta = valor.GetProperty("offerType").ToString();
 
                         //seller
                         sellerJocTemp.IdSeller = IdSellerProva;
-                        sellerJocTemp.NomSeller = valor.GetProperty("seller").GetProperty("name").ToString();
+                        FreeGamesBuilders.SellerBuilder(sellerJocTemp, valor);
 
                         //joc
                         jocTemp.IdJoc = IdJocProva;
-                        jocTemp.Title = valor.GetProperty("title").ToString();
-                        jocTemp.Tipus = tipusOferta;
+                        FreeGamesBuilders.JocBuilder(jocTemp, valor);
 
                         //jocenplataforma
                         jocEnPlataformaTemp.IdJocPlatataforma = IdJEPProva;
-                        jocEnPlataformaTemp.Desc = valor.GetProperty("description").ToString();
-
-                        //jocenplataforma enllaç
-                        try
-                        {
-                            if (tipusOferta == "BASE_GAME")
-                            {
-                                //anar amb cuidado amb aquest [0], algo em diu que epic no te un array d'objectes alla, per no fer res
-                                var fragmentOferta = valor.GetProperty("catalogNs").GetProperty("mappings")[0].GetProperty("pageSlug").ToString();
-                                jocEnPlataformaTemp.Enllaç = $"https://store.epicgames.com/p/{fragmentOferta}";
-                            }
-                            else if (tipusOferta == "ADD_ON")
-                            {
-                                //anar amb cuidado amb aquest [0], algo em diu que epic no te un array d'objectes alla, per no fer res
-                                var fragmentOferta = valor.GetProperty("offerMappings")[0].GetProperty("pageSlug").ToString();
-                                jocEnPlataformaTemp.Enllaç = $"https://store.epicgames.com/p/{fragmentOferta}";
-                            }
-                            else if (tipusOferta == "BUNDLE")
-                            {
-                                //potser fer peticio amb un webclient per veure que realment urlslug sempre furuli?, de moment no ja que confio amb les meves estimacions.
-                                var fragmentOferta = valor.GetProperty("urlSlug").ToString();
-                                jocEnPlataformaTemp.Enllaç = $"https://store.epicgames.com/bundles/{fragmentOferta}";
-                            }
-                            else
-                            {
-                                throw new Exception();
-                                //en comptes de tirar excepcio, podriam provar de quedar-nos amb
-                                //per exemple urlslug i catalogns, i si despres de montar i fer peticions no va tirar excepcio...
-                            }
-
-                        }
-                        catch
-                        {
-                            Console.WriteLine($"el tipus {tipusOferta} en la plataforma: X, no esta gestionat, si no es aixo, espavila");
-                        }
-
-                        var text = valor.GetProperty("price").GetProperty("totalPrice").GetProperty("fmtPrice").GetProperty("originalPrice").ToString();
-                        var preu = Convert.ToDecimal(text.Replace("$", "").Trim());
-                        jocEnPlataformaTemp.PreuOriginal = preu;
-
-                        //jocEnPlataformaTemp.ImatgeLink
-                        jocEnPlataformaTemp.ImatgeLink = valor.GetProperty("keyImages")
-                            .EnumerateArray()
-                            .First(e => e.GetProperty("type")
-                            .ToString() == "Thumbnail")
-                            .GetProperty("url")
-                            .ToString();
-
-                        //DadesJson
-                        ofertaTemp.DadesJsonOferta = valor.ToString();
+                        FreeGamesBuilders.JocEnPlataformaBuilder(jocEnPlataformaTemp, valor);
 
                         //oferta
-                        ofertaTemp.IdExtretOferta = valor.GetProperty("id").ToString();
-                        ofertaTemp.Descompte = 100;
-
-                        var promotions = valor.GetProperty("promotions");
-
-                        try
-                        {
-                            if (promotions.GetProperty("promotionalOffers").GetArrayLength() != 0)
-                            {
-                                ofertaTemp.DataIniciOferta = DateTimeOffset.Parse(promotions
-                                    .GetProperty("promotionalOffers")[0]
-                                    .GetProperty("promotionalOffers")[0]
-                                    .GetProperty("startDate")
-                                    .ToString());
-
-                                ofertaTemp.DataFiOferta = DateTimeOffset.Parse(promotions
-                                    .GetProperty("promotionalOffers")[0]
-                                    .GetProperty("promotionalOffers")[0]
-                                    .GetProperty("endDate")
-                                    .ToString());
-                            }
-                            else if (promotions.GetProperty("upcomingPromotionalOffers").GetArrayLength() != 0)
-                            {
-                                ofertaTemp.DataIniciOferta = DateTimeOffset.Parse(promotions
-                                    .GetProperty("upcomingPromotionalOffers")[0]
-                                    .GetProperty("promotionalOffers")[0]
-                                    .GetProperty("startDate")
-                                    .ToString());
-
-                                ofertaTemp.DataFiOferta = DateTimeOffset.Parse(promotions
-                                    .GetProperty("upcomingPromotionalOffers")[0]
-                                    .GetProperty("promotionalOffers")[0]
-                                    .GetProperty("endDate")
-                                    .ToString());
-                            }
-                            else
-                            {
-                                throw new Exception();
-                            }
-                        }
-                        catch
-                        {
-                            Console.WriteLine("VINGA A IMPLEMENTAR TRYX LETSGOO!!!");
-                        }
-
-                        ofertaTemp.esGratis = true;
-
-                        ofertaTemp.PreuMomentOferta = preu;
-
-
-                        ////relacions joc-seller
-                        //jocTemp.IdSeller = sellerJocTemp.IdSeller;
-                        //jocTemp.Seller = 
-                        //sellerJocTemp.Jocs.Add(jocTemp);
-
-
-
-                        ////relacio joc-jocenplataforma
-                        //jocEnPlataformaTemp.IdJoc = jocTemp.IdJoc;
-
-                        ////relacio plataforma-jocenplataforma
+                        FreeGamesBuilders.OfertaBuilder(ofertaTemp, valor);
 
                         Console.WriteLine($"iteracio {IdSellerProva}");
                         Console.WriteLine(sellerJocTemp.ToString());
--- Backend-GameDiscountNotifier/Logic/FreeGamesBuilders.cs
+++ Backend-GameDiscountNotifier/Logic/FreeGamesBuilders.cs
@@ -5,11 +5,119 @@ namespace Backend_GameDiscountNotifier.Logic
 {
     public class FreeGamesBuilders
     {
-        //metodes que tindran logica de forma dividida de BackgroundServiceAdquireFreeGamesJSON
-        public static void SellerBuilder(SellerJoc sellerJocTemp, JsonElement valor) { }
-        public static void JocBuilder(Joc jocTemp, JsonElement valor) { }
-        public static void JocEnPlataformaBuilder(JocEnPlataforma jocEnPlataformaTemp, JsonElement valor) { }
-        public static void OfertaBuilder(Oferta ofertaTemp, JsonElement valor) { }
+        //pasar els metodes voids perque tornin un objecte?
+        public static void SellerBuilder(SellerJoc sellerJocTemp, JsonElement valor)
+        {
+            sellerJocTemp.NomSeller = valor.GetProperty("seller").GetProperty("name").ToString();
+        }
+        public static void JocBuilder(Joc jocTemp, JsonElement valor)
+        {
+            jocTemp.Title = valor.GetProperty("title").ToString();
+            jocTemp.Tipus = valor.GetProperty("offerType").ToString();
+        }
+        public static void JocEnPlataformaBuilder(JocEnPlataforma jocEnPlataformaTemp, JsonElement valor)
+        {
+            jocEnPlataformaTemp.Desc = valor.GetProperty("description").ToString();
+            jocEnPlataformaTemp.PreuOriginal = AconseguirPreu(valor);
+            jocEnPlataformaTemp.Enllaç = ExtreureEnllacTipusOferta(valor);
+            jocEnPlataformaTemp.ImatgeLink = valor.GetProperty("keyImages")
+                .EnumerateArray()
+                .First(e => e.GetProperty("type")
+                .ToString() == "Thumbnail")
+                .GetProperty("url")
+                .ToString();
+        }
+        public static void OfertaBuilder(Oferta ofertaTemp, JsonElement valor)
+        {
+            ofertaTemp.IdExtretOferta = valor.GetProperty("id").ToString();
+            ofertaTemp.Descompte = 100;
+            ofertaTemp.DataIniciOferta = LogicaData(valor, "startDate");
+            ofertaTemp.DataFiOferta = LogicaData(valor, "endDate");
+            ofertaTemp.esGratis = true;
+            ofertaTemp.PreuMomentOferta = AconseguirPreu(valor);
+            ofertaTemp.DadesJsonOferta = valor.ToString();
+        }
+        public static decimal AconseguirPreu(JsonElement valor)
+        {
+            var text = valor.GetProperty("price")
+                .GetProperty("totalPrice")
+                .GetProperty("fmtPrice")
+                .GetProperty("originalPrice")
+                .ToString();
 
+            return Convert.ToDecimal(text.Replace("$", "").Trim());
+        }
+        public static string ExtreureEnllacTipusOferta(JsonElement valor)
+        {
+            var tipusOferta = valor.GetProperty("offerType").ToString();
+
+            if (tipusOferta == "BASE_GAME")
+            {
+                //anar amb cuidado amb aquest [0], algo em diu que epic no te un array d'objectes alla, per no fer res
+                var fragmentOferta = valor.GetProperty("catalogNs")
+                    .GetProperty("mappings")[0]
+                    .GetProperty("pageSlug")
+                    .ToString();
+
+                return $"https://store.epicgames.com/p/{fragmentOferta}";
+            }
+            else if (tipusOferta == "ADD_ON")
+            {
+                //anar amb cuidado amb aquest [0], algo em diu que epic no te un array d'objectes alla, per no fer res
+                var fragmentOferta = valor
+                    .GetProperty("offerMappings")[0]
+                    .GetProperty("pageSlug")
+                    .ToString();
+
+                return $"https://store.epicgames.com/p/{fragmentOferta}";
+            }
+            else if (tipusOferta == "BUNDLE")
+            {
+                //potser fer peticio amb un webclient per veure que realment urlslug sempre furuli?, de moment no ja que confio amb les meves estimacions.
+                var fragmentOferta = valor.GetProperty("urlSlug").ToString();
+                return $"https://store.epicgames.com/bundles/{fragmentOferta}";
+            }
+            else
+            {
+                throw new Exception();
+                //en comptes de tirar excepcio, podriam provar de quedar-nos amb
+                //per exemple urlslug i catalogns, i si despres de montar i fer peticions no va tirar excepcio...
+            }
+        }
+        public static DateTimeOffset LogicaData(JsonElement valor, string tipusdedata)
+        {
+            var promotions = valor.GetProperty("promotions");
+
+            if (promotions.GetProperty("promotionalOffers").GetArrayLength() != 0)
+            {
+                return DateTimeOffset.Parse(extreureData(valor, "promotionalOffers", "promotionalOffers", tipusdedata));
+            }
+            else if (promotions.GetProperty("upcomingPromotionalOffers").GetArrayLength() != 0)
+            {
+                return DateTimeOffset.Parse(extreureData(valor, "upcomingPromotionalOffers", "promotionalOffers", tipusdedata));
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+        public static string extreureData(JsonElement valor, string arrel1, string arrel2, string arrel3Definit)
+        {
+            const string PROMOTIONS = "promotions";
+
+            return valor
+                .GetProperty(PROMOTIONS)
+                .GetProperty(arrel1)[0]
+                .GetProperty(arrel2)[0]
+                .GetProperty(arrel3Definit)
+                .ToString();
+        }
+        //public static void EpicGameMapper
+        //(
+        //    SellerJoc sellerJocTemp, 
+        //    Joc jocTemp, 
+        //    JocEnPlataforma jocEnPlataformaTemp, 
+        //    Oferta ofertaTemp, 
+        //    JsonElement valor) {}
     }
 }
